269999ab4571148adeb92829fc2670c72d17464d
Ntsikelelo Metseeme <nmetseeme@dearx.co.za>
2021-10-19 12:23:24 +0200

    Bad Request Error

--- ContactDetailsDemo/src/main/java/za/co/dearx/ContactDetailsDemo/resource/ContactDetailResource.java
+++ ContactDetailsDemo/src/main/java/za/co/dearx/ContactDetailsDemo/resource/ContactDetailResource.java
@@ -12,4 +12,13 @@ public class ContactDetailResource {
 	public ResponseEntity<String> success() {
 		return ResponseEntity.ok("Successful response");
 	}
+
+	/*
+	*Error 400 part of the 4XX request 
+	*Curl test: curl  http://localhost:8080/api/badRequest -v
+	*/
+	@GetMapping("/badRequest")
+	public ResponseEntity<String> badRequest() {
+		return ResponseEntity.badRequest().body("Bad Request response");
+	}
 }
